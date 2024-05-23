ca5d30b5f0fb2b38cefe0eb1514ad57a9f29c50e
danhenrik <danihtoledo22@gmail.com>
2024-05-22 23:22:15 -0300

    fix: fix reliability issues

--- src/main/java/com/github/mauricioaniche/ck/util/FileUtils.java
+++ src/main/java/com/github/mauricioaniche/ck/util/FileUtils.java
@@ -2,47 +2,49 @@ package com.github.mauricioaniche.ck.util;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class FileUtils {
-	//Get all directories from the directory at the given path.
+    //Get all directories from the directory at the given path.
 	public static String[] getAllDirs(String path) {
-		try {
-			return Files.walk(Paths.get(path))
+		try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+			return paths
 					.filter(Files::isDirectory)
 					.filter(x -> !isGitDir(x.toAbsolutePath().toString()))
 					.map(x -> x.toAbsolutePath().toString())
 					.toArray(String[]::new);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	//Get all java class files from the directory at the given path.
-	public static String[] getAllJavaFiles(String path) {
-		return getAllFiles(path, "java");
-	}
+    //Get all java class files from the directory at the given path.
+    public static String[] getAllJavaFiles(String path) {
+        return getAllFiles(path, "java");
+    }
 
-	//Get all jars from the directory at the given path.
-	public static String[] getAllJars(String path) {
-		return getAllFiles(path, "jar");
-	}
+    //Get all jars from the directory at the given path.
+    public static String[] getAllJars(String path) {
+        return getAllFiles(path, "jar");
+    }
 
-	//Get all files from of the given file ending from the directory at the given path.
-	private static String[] getAllFiles(String path, String ending){
-		try {
-			return Files.walk(Paths.get(path))
-					.filter(Files::isRegularFile)
-					.filter(x -> !isGitDir(x.toAbsolutePath().toString()))
-					.filter(x -> x.toAbsolutePath().toString().toLowerCase().endsWith(ending))
-					.map(x -> x.toAbsolutePath().toString())
-					.toArray(String[]::new);
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    //Get all files from of the given file ending from the directory at the given path.
+    public static String[] getAllFiles(String path, String ending) {
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+            return paths
+                    .filter(Files::isRegularFile)
+                    .filter(x -> !isGitDir(x.toAbsolutePath().toString()))
+                    .filter(x -> x.toAbsolutePath().toString().toLowerCase().endsWith(ending))
+                    .map(x -> x.toAbsolutePath().toString())
+                    .toArray(String[]::new);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	//Is the given directory a git repository directory?
-	private static boolean isGitDir(String path){
-		return path.contains("/.git/");
-	}
+    //Is the given directory a git repository directory?
+    private static boolean isGitDir(String path) {
+        return path.contains("/.git/");
+    }
 }
